1b49d73877d034441052b62b462a58114de90810
Nafas Ebrahimi <ebranafas@gmail.com>
2023-09-18 12:23:29 +0330

    fix bugs about component path and change metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
-import Image from 'next/image'
-import { Footer, Navbar } from '../components'
+import { Footer, Navbar, Header } from "@/components/index";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
-
+      <Header />
       <Footer />
     </main>
-  )
+  );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Container } from "./Container";
 export { default as Navbar } from "./Navbar";
+export { default as Header } from "./Header";
 export { default as Footer } from "./Footer";
