62e7f199b41d20e130936d4b77bc4e7349b6aa4d
Nafas Ebrahimi <ebranafas@gmail.com>
2023-09-18 18:17:19 +0330

    add some items to header component and set alias to next config file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const path = require('path');
+
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias['@/components'] = path.join(__dirname, 'src/components');
+    config.resolve.alias['@/assets'] = path.join(__dirname, 'src/assets');
+    return config;
+  },
+}
 
 module.exports = nextConfig
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 
 import Style from './Header.module.css'
+import { Container } from '..'
 
 function Header() {
   return (
-    <div>index</div>
+    <header className={Style.header}>
+      <Container otherClass="flex flex-col gap-10 py-16 min-h-screen">
+        <div className='w-full max-w-[892px] mx-auto'>
+          <h1 className='text-[70px] leading-[75px] font-semibold'>
+            Where Web3 
+            <br />
+            Gaming History Begins
+          </h1>
+          <p className='mt-8 text-light font-light'>
+            Be Among the First Creators on 3engine
+          </p>
+        </div>
+      </Container>
+    </header>
   )
 }
 
