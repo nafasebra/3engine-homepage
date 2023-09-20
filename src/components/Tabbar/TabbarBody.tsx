802ea8d241cf6a7428b5c2385219cd4ff7b1cf60
Nafas Ebrahimi <ebranafas@gmail.com>
2023-09-20 11:53:29 +0330

    responsive footer and tabbar component

--- src/components/Tabbar/TabbarBody.tsx
+++ src/components/Tabbar/TabbarBody.tsx
@@ -1,29 +1,39 @@
-import { EmailIcon, EmailStarIcon, SendIcon } from '@/assets/icons'
-import React from 'react'
+import { EmailIcon, EmailStarIcon, SendIcon } from "@/assets/icons";
+import React from "react";
+
+import Style from "./Tabbar.module.css";
 
 function TabbarBody() {
   return (
-    <form className='relative py-8 pl-8 pr-16 rounded-b-2xl rounded-tr-2xl bg-white overflow-hidden flex flex-col lg:flex-row items-center gap-8'>
-      <div className='w-full lg:w-[40%] flex flex-col gap-3'>
-        <label className='flex items-center gap-3 text-black'>
+    <form className={Style.tabbar_body}>
+      <div className={Style.tabbar_input__container}>
+        <label className={Style.tabbar_input__label}>
           <EmailIcon />
-          <p className='font-semibold'>Your Email</p>
+          <p className="font-semibold">Your Email</p>
         </label>
-        <input type="text" className='border-none outline-none text-gray-900' placeholder='example@gmail.com' />
+        <input
+          type="text"
+          className={Style.tabbar_input__textbox}
+          placeholder="example@gmail.com"
+        />
       </div>
-      <div className='w-full lg:w-[40%] flex flex-col gap-3'>
-        <label className='flex items-center gap-3 text-black'>
+      <div className={Style.tabbar_input__container}>
+        <label className={Style.tabbar_input__label}>
           <EmailStarIcon />
-          <p className='font-semibold'>Who invited you?</p>
+          <p className="font-semibold">Who invited you?</p>
         </label>
-        <input type="text" className='border-none outline-none text-gray-900' placeholder='example@gmail.com' />
+        <input
+          type="text"
+          className={Style.tabbar_input__textbox}
+          placeholder="example@gmail.com"
+        />
       </div>
-      <button type='submit' className='absolute top-0 right-0 bottom-0 text-white bg-primary flex flex-row lg:flex-col items-center lg:items-start justify-center gap-3 w-full lg:w-auto px-4 lg:px-10 shadow-[-46px_15px_42px_rgba(0,120,242,0.35)] hover:bg-primary/75 transition-colors'>
+      <button type="submit" className={Style.tabbar_submit}>
         <SendIcon />
-        <p className='text-lg'>Join</p>
+        <p className="text-lg">Join</p>
       </button>
     </form>
-  )
+  );
 }
 
-export default TabbarBody
+export default TabbarBody;
